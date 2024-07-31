28dac9295b5237d26e5dbdeebb205d9f6e48b127
Wesley Song <wes.song@gmail.com>
2024-07-30 22:26:58 -0600

    Initial site setup with routing to home and resume components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { ResumeComponent } from './pages/resume/resume.component';
+
+export const routes: Routes = [
+  {
+    component: HomeComponent,
+    path: ''
+  },
+  {
+    component: ResumeComponent,
+    path: 'resume'
+  }
+];
